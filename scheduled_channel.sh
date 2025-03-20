3da9cbef97a6ae1db8335c8e80e89ff094780b9c
iippam <fahmie.junior@gmail.com>
2025-03-21 06:05:10 +0700

    Scheduled commit: Fri Mar 21 06:05:10 AM WIB 2025

--- scheduled_channel.sh
+++ scheduled_channel.sh
@@ -11,7 +11,7 @@ python3 NET.py > ../NET.m3u8
 python3 metrotv.py > ../MetroTV.m3u8
 python3 inews.py > ../inews.m3u8
 python3 tvone.py > ../tvOne.m3u8
-
+python3 liputan6.py > ../liputan6.m3u8
 
 echo m3u grabbed
 
