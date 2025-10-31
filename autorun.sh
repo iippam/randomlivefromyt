b6b168d2f0bbaefb95bbafff9a448220774497c2
iippam <fahmie.junior@gmail.com>
2025-11-01 06:50:09 +0700

    Scheduled commit: Sat Nov  1 06:50:09 AM WIB 2025

--- autorun.sh
+++ autorun.sh
@@ -15,6 +15,9 @@ python3 metrotv.py > ../MetroTV.m3u8
 echo grabbing liputan6 Streaming URL
 python3 liputan6.py > ../liputan6.m3u8
 
+echo grabbing tvOne Streaming URL
+python3 tvone.py > ../tvOne.m3u8
+
 echo grabbing NET Streaming URL
 python3 NET.py > ../NET.m3u8
 
--- scheduled_channel.sh
+++ scheduled_channel.sh
@@ -6,17 +6,23 @@ echo $(dirname $0)
 
 cd $(dirname $0)/scripts/
 
+echo getting URL KompasTV ..
 python3 KompasTV.py > ../KompasTV.m3u8
+echo getting URL NET ..
 python3 NET.py > ../NET.m3u8
+echo getting URL MetroTV ..
 python3 metrotv.py > ../MetroTV.m3u8
+echo getting URL iNews ..
 python3 inews.py > ../inews.m3u8
+echo getting URL tvOne
 python3 tvone.py > ../tvOne.m3u8
+echo getting URL Liputan6 ..
 python3 liputan6.py > ../liputan6.m3u8
 
 echo m3u grabbed
 
-#./git_schedule.sh
-cd /root/
-./git_test.sh
+./git_schedule.sh
+#cd /root/
+#./git_test.sh
 
 echo push and commit
